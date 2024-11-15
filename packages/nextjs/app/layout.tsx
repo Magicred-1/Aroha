f02078024ecd1feffcd89423a94697c241d27595
Lucas Pereira <lucas.pereira@intercellar.io>
2024-11-16 03:23:09 +0700

    remove useless code

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,34 +1,19 @@
-
 import { Footer } from "@/components/Footer";
 import { SidebarComponent } from "@/components/SidebarComponent";
-import MainProvider from "@/components/web3auth/MainProvider";
+import { Providers } from "@/components/web3auth/provider/providers";
 import "@/styles/globals.css";
-import { getMetadata } from "@/utils/scaffold-eth/getMetadata";
-import "@rainbow-me/rainbowkit/styles.css";
-import {
-  hederaTestnet,
-  lineaSepolia,
-  rootstockTestnet,
-  scrollSepolia,
-  unichainSepolia,
-  zircuitTestnet,
-} from "viem/chains";
-
-export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <body>
-        <MainProvider>
+        <Providers>
           <div className="flex h-screen">
             <SidebarComponent />
-
             <div className="flex-1 p-6 bg-gradient-to-l from-gray-900 to-black">{children}</div>
-            
           </div>
-          <Footer/>
-        </MainProvider>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
