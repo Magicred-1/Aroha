9a095a739e66fd5bf834dc8dc3893fb2abd56840
Lucas Pereira <lucas.pereira@intercellar.io>
2024-11-16 22:01:22 +0700

    feat: disable sidebar in login page

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,28 +1,20 @@
-
+import { useAccountEffect } from "wagmi";
 import { Footer } from "@/components/Footer";
 import { SidebarComponent } from "@/components/SidebarComponent";
-import { Navbar } from "@/components/ui/navbar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { Providers } from "@/components/web3auth/provider/providers";
 import "@/styles/globals.css";
 
-
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
-
   return (
     <html suppressHydrationWarning>
       <body>
         <Providers>
-          <SidebarProvider>
-            <div className="flex flex-col min-h-screen">
-              <Navbar />
-              <div className="flex flex-1 overflow-hidden">
-                <SidebarComponent />
-                <main className="flex-1 overflow-y-auto p-6 bg-gradient-to-l from-gray-900 to-black">{children}</main>
-              </div>
-              <Footer />
+          <div className="flex flex-col h-screen">
+            <div className="flex flex-1">
+              <main className="flex-1 bg-gradient-to-l from-gray-900 to-black">{children}</main>
             </div>
-          </SidebarProvider>
+            <Footer />
+          </div>
         </Providers>
       </body>
     </html>
