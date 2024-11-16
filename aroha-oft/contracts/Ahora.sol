c4c82097d9051df312078638e07fdeb26f0af5df
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-11-16 13:09:08 +0700

    Ahora

--- aroha-oft/contracts/Ahora.sol
+++ aroha-oft/contracts/Ahora.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import { MyOFT } from "./MyOFT.sol";
+import { MyOTF } from "./MyOTF.sol";
 
 contract Ahora is Ownable {
     bytes32 public merkleRoot;
 
-    mapping(address => bool) private isWhitelisted;
-    mapping(uint256 => address) private tokensAddresses;
-
+    mapping(address => bool) private whitelistedAddresses;
+    
     constructor(address _initialOwner, bytes32 _merkleRoot)
         Ownable(_initialOwner)
     {
@@ -25,11 +24,22 @@ contract Ahora is Ownable {
         
     // }
 
-    function whitelistAddress(bytes32 _root, bytes32 _leaf) public pure {
-        MerkleProof.verify(_root, _leaf, 0);
+   function whitelist(
+        bytes32[] memory proof,
+        address addr
+    ) public {
+        // Step 1: Generate the leaf node
+        bytes32 leaf = keccak256(abi.encode(addr));
+
+        // Step 2: Verify the proof
+        require(MerkleProof.verify(proof, merkleRoot, leaf), "Invalid proof");
+
+        // Step 3: Mark the address as whitelisted
+        whitelistedAddresses[addr] = true;
     }
 
-    function isWhitelistedAddress(address _address) public view returns (bool) {
-        return isWhitelisted[_address];
+    function isWhitelisted(address _account, bytes32[] calldata _proof) internal view returns(bool) {
+        bytes32 leaf = keccak256(abi.encode(keccak256(abi.encode(_account))));
+        return MerkleProof.verify(_proof, merkleRoot, leaf);
     }
 }
