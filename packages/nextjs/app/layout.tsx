be3b585946d7d8be1dfd2be888a589092cb93e7d
Sakshi Shah <sakshi.shah299@gmail.com>
2024-11-17 00:42:10 +0700

    fix: marketplace changes

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,17 +1,28 @@
+
 import { Footer } from "@/components/Footer";
 import { SidebarComponent } from "@/components/SidebarComponent";
+import { Navbar } from "@/components/ui/navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Providers } from "@/components/web3auth/provider/providers";
 import "@/styles/globals.css";
 
+
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+
   return (
     <html suppressHydrationWarning>
       <body>
         <Providers>
-          <div className="flex h-screen">
-            <div className="flex-1 bg-gradient-to-l from-gray-900 to-black">{children}</div>
-          </div>
-          <Footer />
+          <SidebarProvider>
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              <div className="flex flex-1 overflow-hidden">
+                <SidebarComponent />
+                <main className="flex-1 overflow-y-auto p-6 bg-gradient-to-l from-gray-900 to-black">{children}</main>
+              </div>
+              <Footer />
+            </div>
+          </SidebarProvider>
         </Providers>
       </body>
     </html>
